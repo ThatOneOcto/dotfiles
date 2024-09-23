4a3d0a01d94e620e5756b8e69af7e977d0b77001
octo <octo@thatoneocto.online>
2024-09-23 15:44:54 -0400

    changes

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -64,8 +64,10 @@ end
 client.connect_signal("property::floating", function(c)
     if c.floating then
         awful.titlebar.show(c)
+	c.ontop = true
     else
         awful.titlebar.hide(c)
+	c.ontop = false
     end
 end)
 
@@ -477,7 +479,10 @@ awful.rules.rules = {
           "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
           "Wpa_gui",
           "veromix",
-          "xtightvncviewer"},
+          "xtightvncviewer",
+  	  "Thunar",
+	  "Kleopatra",
+  	  },
 
         -- Note that the name property shown in xprop might be set slightly after creation of the client
         -- and the name shown there might not match defined rules here.
